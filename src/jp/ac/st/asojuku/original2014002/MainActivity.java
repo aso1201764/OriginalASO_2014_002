0c75e8760498fb662d9a655e03e261e699c92400
aso1201728 <1201728@st.asojuku.ac.jp>
2014-06-06 10:56:42 +0900

    2014/06/06 金曜日　一限目

--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -1,9 +1,11 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -17,12 +19,46 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
+		// ボタンをIDで探してボタン変数をつくる
+		Button  btn_mente = (Button)findViewById(R.id.btn_mente);
+		btn_mente.setOnClickListener(this);
+
+		Button btn_touroku = (Button)findViewById(R.id.btn_touroku);
+		btn_touroku.setOnClickListener(this);
+
+		Button btn_check = (Button)findViewById(R.id.btn_check);
+		btn_check.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
-		super.onResume();
+
+		// 生成して代入用のIntentインスタンス変数を用意
+		Intent intent = null;
+		switch(v.getId()){	//どのボタンが押されたか判定
+			case R.id.btn_mente:	// btn_menteが押された
+				//	インテントのインスタンスを生成
+				intent = new Intent(MainActivity.this, MaintenanceActivity.class);
+				// 次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+
+			case R.id.btn_touroku:	// btn_tourokuが押された
+
+
+
+				break;
+
+
+			case R.id.btn_check:	// btn_checkが押された
+				// インテントのインスタンスを生成
+				intent = new Intent(MainActivity.this, HitokotoActivity.class);
+				// 次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+
+		}
 
 
 	}
--- src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
@@ -1,15 +1,50 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MaintenanceActivity extends Activity {
+public class MaintenanceActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maintenance);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		// ボタンをIDで探してボタン変数をつくる
+		Button btn_back = (Button)findViewById(R.id.btn_back);
+		btn_back.setOnClickListener(this);
+
+		Button btn_delete = (Button)findViewById(R.id.btn_delete);
+		btn_delete.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		// 生成して代入用のIntentインスタンス変数を用意
+		Intent intent = null;
+		switch(v.getId()){	//どのボタンが押されたか判定
+			case R.id.btn_back:	//btn_backが押された
+				// インテントのインスタンスを生成
+				intent = new Intent(MaintenanceActivity.this, MainActivity.class);
+				// 次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+
+			case R.id.btn_delete:
+
+
+				break;
+		}
 	}
 
 }
