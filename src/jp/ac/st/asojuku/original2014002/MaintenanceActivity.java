076a94df9b6911f8506c3c898c6ed805083628d4
aso1201728 <1201728@st.asojuku.ac.jp>
2014-06-16 17:25:33 +0900

    2014/06/16 4th

--- src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
@@ -2,11 +2,29 @@ package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class MaintenanceActivity extends Activity implements View.OnClickListener {
+public class MaintenanceActivity extends Activity implements
+	View.OnClickListener, AdapterView.OnItemClickListener {
+
+	// SQLiteデータベース空間を操作するインスタンス変数を宣言
+	SQLiteDatabase sdb = null;
+	// MySQLiteOpenHelperを操作するインスタンス変数を宣言
+	MySQLiteOpenHelper helper = null;
+
+	// リストにて選択したHitokotoテーブルのレコードの「_id」カラム値を保持する変数の宣言
+	int selectedID = -1;
+	// リストにて選択した行番号を保持する変数の宣言
+	int lastPosition = -1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,48 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 
 		Button btn_delete = (Button)findViewById(R.id.btn_delete);
 		btn_delete.setOnClickListener(this);
+
+		ListView lst_Hitokoto = (ListView)findViewById(R.id.lst_Hitokoto);
+		lst_Hitokoto.setOnItemClickListener(this);
+
+		// ListViewにDBの値をセット
+		this.setDBValuetoList(lst_Hitokoto);
+	}
+
+
+
+	private void setDBValuetoList(ListView lst_Hitokoto) {
+
+		SQLiteCursor cursor = null;
+
+		// クラスのフィールド変数がNULLなら、データベース空間オープン
+		if(sdb == null) {
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try {
+			sdb = helper.getWritableDatabase();
+		} catch(SQLiteException e) {
+			// 異常終了
+			Log.e("ERROR", e.toString());
+		}
+		// MySQLiteOpenHelperにSELECT文を実行させて結果のカーソルを受け取る
+		cursor = this.helper.selectHitokotoList(sdb);
+
+		// dblayout: ListViewにさらにレイアウトを指定するもの
+		int db_layout = android.R.layout.simple_list_item_activated_1;
+		// from: カーソルからListViewに指定するカラムの値を指定するもの
+		String[]from = {"phrase"};
+		// to: Listviewの中に指定したdb_layoutに配置する、各行のview部品のid
+		int[] to = new int[]{android.R.id.text1};
+
+		// ListViewにセットするアダプターを生成
+		// カーソルをもとに、fromの列から、toのViewへ値のマッピングが行われる
+		SimpleCursorAdapter adapter =
+				new SimpleCursorAdapter(this,db_layout,cursor,from,to,0);
+
+		// アダプターを設定します
+		lst_Hitokoto.setAdapter(adapter);
+
 	}
 
 	@Override
@@ -45,6 +105,12 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 
 				break;
 		}
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		// TODO 自動生成されたメソッド・スタブ
+
 	}
 
 }
